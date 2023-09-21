9c14af959e310fe0e1febfe636351cc00fa7ddf9
jong6598 <108744804+jong6598@users.noreply.github.com>
2023-09-21 18:18:17 +0900

    setting: 프로젝트 초기 셋팅

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+      <div style={{ marginRight: '100px' }}>
+        <h1>룩키즈</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
