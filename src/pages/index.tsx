5a7fd271846189ffacde8201564cad4e3f68651f
jong6598 <108744804+jong6598@users.noreply.github.com>
2023-09-22 15:42:16 +0900

    fix: 메인페이지 이번주 랭킹 UI 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import { LankView } from '@/components/templates/LankView/LankView';
+import Image from 'next/image';
+
 const Home = () => {
   return (
-    <div>
-      <h1>이번주 랭킹</h1>
+    <div className="max-w-md px-5">
+      <div className="flex items-center pb-2">
+        <h1 className="font-bold text-[20px] mr-0.5">이번주 랭킹</h1>
+        <Image src={'/img/rank10.png'} width={30} height={30} alt="" priority />
+      </div>
+      <LankView />
     </div>
   );
 };
