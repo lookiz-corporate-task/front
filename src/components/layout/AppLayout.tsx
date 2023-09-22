cd2cfd1ef8ea979a0e98a96352d99d1e1bd42999
jong6598 <108744804+jong6598@users.noreply.github.com>
2023-09-22 18:18:23 +0900

    style: 네비게이션바 사이즈 수정

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -6,7 +6,9 @@ const AppLayout = (props: { children: React.ReactNode }) => {
     <div className={'flex flex-col justify-center items-center bg-[#f9f2e7]'}>
       <div className={'max-w-md bg-white'}>
         <Header />
-        <div className={'h-[calc(100vh-132px)] text-black'}>{props.children}</div>
+        <div className={'top-[52px] h-[calc(100vh-112px)] text-black overflow-y-auto'}>
+          {props.children}
+        </div>
         <div className="z-10 max-w-md h-[80px]">
           <NavBar />
         </div>
