3c0ca0ffcbbb638525d8bf0f49727ac666a745be
jong6598 <108744804+jong6598@users.noreply.github.com>
2023-09-23 01:12:34 +0900

    fix: 로그인 페이지 레이아웃 변경을 위한 코드 수정

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,7 +1,19 @@
+import { usePathname } from 'next/navigation';
 import { Header } from '../common/Header';
 import { NavBar } from '../common/NavBar';
 
 const AppLayout = (props: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
+  if (pathname === '/login')
+    return (
+      <div className={'flex flex-col justify-center items-center bg-[#f9f2e7]'}>
+        <div className={'max-w-md h-full bg-white'}>
+          <div className={'h-[100vh] max-w-md text-black overflow-y-auto'}>{props.children}</div>
+        </div>
+      </div>
+    );
+
   return (
     <div className={'flex flex-col justify-center items-center bg-[#f9f2e7]'}>
       <div className={'max-w-md h-full bg-white'}>
