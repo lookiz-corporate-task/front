6349748bc1299a385916e18ea32b24b1060d325f
jong6598 <108744804+jong6598@users.noreply.github.com>
2023-09-24 04:56:52 +0900

    remove: 불필요한 코드 제거

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,6 @@ import type { NextRequest } from 'next/server';
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const email = request.cookies.get('user_email');
-  console.log(email, '@@@@@@');
   if (!email) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
