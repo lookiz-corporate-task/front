eee60ba22e784745980c0ede95cdc7652fa5ee79
jong6598 <108744804+jong6598@users.noreply.github.com>
2023-09-24 05:42:51 +0900

    sytle: 랭킹 UI 수정

--- src/components/templates/LankView/LankView.tsx
+++ src/components/templates/LankView/LankView.tsx
@@ -7,6 +7,8 @@ import { Mousewheel, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { LankItemView } from './LankItem/LankItemView';
 import { Post } from '@/types/post';
+import Champion from '../../../svg/champion.svg';
+import FullHeart from '../../../svg/fullheart.svg';
 
 interface Props {
   rankList: Post[];
@@ -27,9 +29,28 @@ export const LankView = ({ rankList }: Props) => {
           disableOnInteraction: false,
         }}
       >
-        {rankList?.map((rank: Post) => (
+        {rankList?.map((rank: Post, index) => (
           <SwiperSlide className="mb-8" key={rank.id}>
-            <LankItemView pageData={rank} isRanking={true} />
+            {index === 0 || index === 1 || index === 2 ? (
+              <div className="relative flex flex-col items-center">
+                <Champion
+                  className={`${
+                    index === 0
+                      ? 'text-yellow-500'
+                      : index === 1
+                      ? 'text-gray-400'
+                      : ' text-yellow-700'
+                  } absolute top-0 left-0 bg-white`}
+                />
+                <LankItemView pageData={rank} isRanking={true} />
+                <div className="flex flex-row justify-center items-center gap-1">
+                  <FullHeart />
+                  <p>{rank.likeCount}</p>
+                </div>
+              </div>
+            ) : (
+              <LankItemView pageData={rank} isRanking={true} />
+            )}
           </SwiperSlide>
         ))}
       </Swiper>
